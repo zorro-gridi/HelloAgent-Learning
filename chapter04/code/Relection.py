7dac15afe8bc5e163d48ccd83a30b18126dc6639
zorro-gridi <{ID}+{username}@users.noreply.github.com>
2025-11-12 10:27:55 +0800

    chapter04

--- chapter04/code/Relection.py
+++ chapter04/code/Relection.py
@@ -151,8 +151,8 @@ class ReflectionAgent:
         return response_text
 
 
+
 if __name__ == '__main__':
     llm_client = HelloAgentsLLM()
     relection_agent = ReflectionAgent(llm_client)
-
     relection_agent.run('编写一个Python函数，找出1到n之间所有的素数 (prime numbers)')
